b804027faa6ad5ff1e9b0badfd05f94bbe5f9e54
Manas10112004 <152773911+Manas10112004@users.noreply.github.com>
2026-02-18 11:52:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, Extension
-import pybind11
+import sys
 
-# Define the extension module
+# This allows the installer to query metadata without crashing 
+# if pybind11 isn't fully linked yet.
+try:
+    import pybind11
+    include_dirs = [pybind11.get_include()]
+except ImportError:
+    include_dirs = []
+
 cpp_module = Extension(
-    'nexus_cpp',  # The name you will use in Python (import nexus_cpp)
-    sources=['cpp_src/optimizer.cpp'], # Path to your C++ file
-    include_dirs=[pybind11.get_include()],
-    language='c++'
+    'nexus_cpp',
+    sources=['cpp_src/optimizer.cpp'],
+    include_dirs=include_dirs,
+    language='c++',
+    extra_compile_args=['-O3', '-std=c++11'],
 )
 
 setup(
     name='nexus_cpp',
     version='1.0',
     ext_modules=[cpp_module],
-    install_requires=['pybind11'],
+    zip_safe=False,
 )
